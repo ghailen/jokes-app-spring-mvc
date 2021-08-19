03db5999341eff4a610bd0df136ed2f5636645e7
ghailen <ghailenemark@gmail.com>
2021-08-19 12:34:55 +0100

    First commit

---
+++ src/main/java/com/ghailene/jokeapp/services/JokeService.java
@@ -0,0 +1,7 @@
+package com.ghailene.jokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+
+}
---
+++ src/main/java/com/ghailene/jokeapp/services/controllers/JokesController.java
@@ -0,0 +1,22 @@
+package com.ghailene.jokeapp.services.controllers;
+
+import com.ghailene.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "chucknorris";
+
+    }
+}
